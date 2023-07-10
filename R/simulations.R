30645dcefd54c5cf12951a314ab469780e8243f5
Andreas Alfons <alfons@ese.eur.nl>
2023-07-10 16:40:44 +0200

    added argument checks for runif_outside_range() and updated documentation (fixes #17 and #18)

--- R/simulations.R
+++ R/simulations.R
@@ -61,26 +61,81 @@ mixture_sim = function(pct_clusters = c(0.5,0.5) , n = 500, p = 10, delta = 10){
                     do.call(rbind, (X_list)))
 }
 
-#' Outside range
+#' Uniform distribution outside a given range
 #' 
-#' Draw from a multivariate uniform distribution outside range of a data set
+#' Draw from a multivariate uniform distribution outside a given range.
+#' Intuitively speaking, the observations are drawn from a multivariate 
+#' uniform distribution on a hyperrectangle with a hole in the middle (in the 
+#' shape of a smaller hyperrectangle). This is useful, e.g., for adding random 
+#' noise to a data set such that the noise consists of large values that do not 
+#' overlap the initial data.
 #'
-#' @param n number of observations to generate.
-#' @param min numeric vector giving the minimum of each variable of the data set.
-#' @param max numeric vector giving the maximum of each variable of the data set.
-#' @param mult multiplication factor to expand the hyperrectangle around the data 
-#'            (which is given by 'min' and 'max'), e.g., the default value 2 gives 
-#'            a hyperrectangle for which each side is twice as long.  The data are 
-#'            then drawn from a uniform distribution on the expanded hyperrectangle 
-#'            from which the smaller hyperrectangle around the data is cut out.
+#' @param n  an integer giving the number of observations to generate.
+#' @param min  a numeric vector giving the minimum of each variable of the 
+#' initial data set (outside of which to generate random noise).
+#' @param max  a numeric vector giving the maximum of each variable of the 
+#' initial data set (outside of which to generate random noise).
+#' @param mult  multiplication factor to expand the hyperrectangle around the 
+#' initial data (which is given by `min` and `max`). For instance, the default 
+#' value 2 gives a hyperrectangle for which each side is twice as long as the 
+#' range of the initial data.  The data are then drawn from a uniform 
+#' distribution on the expanded hyperrectangle from which the smaller 
+#' hyperrectangle around the data is cut out. See the examples for an 
+#' illustration.
+#' 
 #' @return A matrix of generated points.
+#' 
 #' @author Andreas Alfons
+#'
+#' @examples
+#' ## illustrations for argument 'mult'
+#' 
+#' # draw observations with argument 'mult = 2'
+#' xy2 <- runif_outside_range(1000, min = rep(-1, 2), max = rep(1, 2), 
+#'                            mult = 2)
+#' # each side of the larger hyperrectangle is twice as long as 
+#' # the corresponding side of the smaller rectanglar cut-out
+#' df2 <- data.frame(x = xy2[, 1], y = xy2[, 2])
+#' ggplot(data = df2, mapping = aes(x = x, y = y)) + 
+#'   geom_point()
+#' 
+#' # draw observations with argument 'mult = 4'
+#' xy4 <- runif_outside_range(1000, min = rep(-1, 2), max = rep(1, 2), 
+#'                            mult = 4)
+#' # each side of the larger hyperrectangle is four times as long 
+#' # as the corresponding side of the smaller rectanglar cut-out
+#' df4 <- data.frame(x = xy4[, 1], y = xy4[, 2])
+#' ggplot(data = df4, mapping = aes(x = x, y = y)) + 
+#'   geom_point()
+#' 
 #' @importFrom stats runif
 #' @export
+
 runif_outside_range <- function(n, min = 0, max = 1, mult = 2) {
+  # check number of observations
+  n <- rep(n, length.out = 1L)
+  if(!is.numeric(n) || !is.finite(n) || n < 0L) {
+    stop("number of observations 'n' must be a positive integer")
+  } else n <- as.integer(n)
+  # check supplied range
+  min <- as.numeric(min)
+  p <- length(min)
+  max <- as.numeric(max)
+  if (length(max) != p) stop("'min' and 'max' must have the same length")
+  if (!all(is.finite(min)) || !all(is.finite(max))) {
+    stop("'min' and 'max' may not contain missing or infinite values")
+  }
+  if (any(min >= max)) {
+    stop("values of 'min' must be smaller than the corresponding values of ", 
+         "'max'")
+  }
+  # check multiplication factor
+  mult <- rep(mult, length.out = 1L)
+  if(!is.numeric(mult) || !is.finite(mult) || mult <= 1L) {
+    stop("multiplication factor 'mult' must be larger than 1")
+  }
   # center and dimension of hyperrectangles
-  m <- (max + min) / 2
-  p <- length(m)
+  m <- (min + max) / 2
   # length of sides of hyperrectangle around data
   a <- max - min
   # halved length of sides of expanded hyperrectangle
@@ -90,7 +145,7 @@ runif_outside_range <- function(n, min = 0, max = 1, mult = 2) {
   upper <- m + h
   # draw from uniform distribution on expanded hyperrectangle and reject the 
   # observation if it lies in the smaller hyperrectangle around the data
-  n_ok <- 0
+  n_ok <- 0L
   X <- matrix(NA_real_, nrow = n, ncol = p)
   while (n_ok < n) {
     # draw new point from uniform distribution on expanded hyperrectangle
@@ -99,7 +154,7 @@ runif_outside_range <- function(n, min = 0, max = 1, mult = 2) {
     reject <- all(x >= min & x <= max)
     # add to matrix
     if (!reject) {
-      n_ok <- n_ok + 1
+      n_ok <- n_ok + 1L
       X[n_ok, ] <- x
     }
   }
